4a0d2c11ca1d6ceec433ea21d609236e21a3770e
YarynaKu <yaryna.kushniruk@gmail.com>
2026-01-29 21:24:26 +0100

    Added new file users.js with const

---
+++ data/users.js
@@ -0,0 +1,27 @@
+
+export const validUser = {
+    name: 'YaTestUser',
+    password: 'YaTestPassword!',
+    email: 'testya@gmail.com'
+};
+
+export const invalidUser = {
+    email: 'testyaya@gmail.com',
+    password: 'YayaTestPassword!'
+};
+
+export const registeredUser = {
+    birthDate: '5',
+    birthMonth: 'June',
+    birthYear: '1992',
+    firstName: 'YaTest',
+    lastName: 'User',
+    company: 'Google',
+    address: 'Silicon Valley',
+    address2: 'Guess what',
+    country: 'Canada',
+    state: 'Atlantica',
+    city: 'Toronto',
+    zipcode: '46971',
+    mobileNumber: '0375839284'
+}
