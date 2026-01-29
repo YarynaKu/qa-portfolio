3cbcf0b80a6cbd17259e3b8a7c671c6c1a6f27d1
YarynaKu <yaryna.kushniruk@gmail.com>
2026-01-29 21:37:55 +0100

    Added changes to SignUpPage.js by adding a new menthod expectSignupError

--- pages/SignUpPage.js
+++ pages/SignUpPage.js
@@ -2,17 +2,26 @@ import { expect } from "@playwright/test";
 import CommonActions from "../utils/CommonActions.js";
 
 
-export default class signupPage {
+export default class SignupPage {
     constructor(page) {
+
         this.actions = new CommonActions(page)
+
         this.newUserNameSelector = '[data-qa="signup-name"]'
         this.newUserEmailSelector = '[data-qa="signup-email"]'
+        this.signupButtonSelector = '[data-qa="signup-button"]'
+
+        this.errorMessage = page.getByText('Email Address already exist!')
     }
 
     async signup(name, email){
         await this.actions.fill(this.newUserNameSelector, name)
         await this.actions.fill(this.newUserEmailSelector, email)
-        await this.actions.click('[data-qa="signup-button"]')
+        await this.actions.click(this.signupButtonSelector)
+    }
+
+    async expectSignupError(){
+        await expect(this.errorMessage).toBeVisible()
     }
 }
 
