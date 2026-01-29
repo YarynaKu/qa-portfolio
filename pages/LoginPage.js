2b41b8164557bed581a36725897588b6c8e3892a
YarynaKu <yaryna.kushniruk@gmail.com>
2026-01-29 21:35:02 +0100

    Added changes to Login functionality files by moving variables into a new file and new method in LoginPage class

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -6,15 +6,27 @@ export default class LoginPage {
 
     constructor(page) {
         this.actions = new CommonActions(page)
+
         this.userEmailSelector = '[data-qa="login-email"]'
         this.userPasswordSelector = '[type="password"]'
         this.loginButtonSelector = '[data-qa="login-button"]'
+
+        this.successfulLoginMessage = page.getByText('Login to your account')
+        this.errorMessage = page.getByText('Your email or password is incorrect!')
     }
 
     async login(email, password){
         await this.actions.fill(this.userEmailSelector, email)
         await this.actions.fill(this.userPasswordSelector, password)
         await this.actions.click(this.loginButtonSelector)
+    }
+
+    async expectLoginError(){
+        await expect(this.errorMessage).toBeVisible()
+    }
+
+    async expectSuccessfulLogin(){
+        await expect(this.successfulLoginMessage).toBeVisible()
     }
 }
 
--- tests/LoginTests.spec.js
+++ tests/LoginTests.spec.js
@@ -1,18 +1,12 @@
 import { test, expect } from "@playwright/test"
 import PomManager from "../pages/POM_practise.js";
+import { validUser, invalidUser } from "../data/users.js";
 
 let pm;
 
-let TEST_EMAIL = 'testya@gmail.com';
-let TEST_PASSWORD = 'YaTestPassword!';
-let TEST_USER = 'YaTestUser';
-let INCORR_TEST_PASSWORD = 'YayaTestPassword!';
-let INCORR_TEST_EMAIL = 'testyaya@gmail.com';
-
-
 test.beforeEach(async ({page}) => {
 
-    await page.goto("https://automationexercise.com/")
+    await page.goto("/login")
     await expect(page.getByAltText('Website for automation practice')).toBeVisible()
 
     page.on('popup', async popup => {
@@ -24,23 +18,21 @@ test.beforeEach(async ({page}) => {
     await page.getByRole('button', {name: 'Consent'}).click();
 
     pm = new PomManager(page)
-
 })
 
     test.afterEach(async ({page}) => {
         await page.close()
     })
 
-test.describe.only('Login Tests', () => {
+test.describe('Login Tests', () => {
 
     test(" 2 Login User with correct email and password", async ({page}) => {
 
     await page.getByRole('link', {name:'Signup / Login'}).click()
-    await expect(page.getByText('Login to your account')).toBeVisible();
+    await pm.loginPage.expectSuccessfulLogin();
 
-    await pm.loginPage.login(`${TEST_EMAIL}`, `${TEST_PASSWORD}`)
-
-    await expect(page.getByText(TEST_USER)).toBeVisible()
+    await pm.loginPage.login(validUser.email, validUser.password)
+    await expect(page.getByText(validUser.name)).toBeVisible()
 
 //    await page.getByRole('link', {name: 'Delete Account'}).click()
 //    await expect(page.getByText('Account Deleted!')).toBeVisible()
@@ -50,24 +42,23 @@ test.describe.only('Login Tests', () => {
     test("3 Login User with incorrect email and password", async ({page}) => {
 
     await page.getByRole('link', {name:'Signup / Login'}).click()
-    await expect(page.getByText('Login to your account')).toBeVisible();
+    await pm.loginPage.expectSuccessfulLogin();
 
-    await pm.loginPage.login(INCORR_TEST_EMAIL, INCORR_TEST_PASSWORD)
-
-    await expect(page.getByText('Your email or password is incorrect!')).toBeVisible()
+    await pm.loginPage.login(invalidUser.email, invalidUser.password)
+    await pm.loginPage.expectLoginError()
 
     })
 
     test("4 Logout User", async ({page}) => {
 
         await page.getByRole('link', {name:'Signup / Login'}).click()
-        await expect(page.getByText('Login to your account')).toBeVisible();
+        await pm.loginPage.expectSuccessfulLogin();
 
-        await pm.loginPage.login(TEST_EMAIL, TEST_PASSWORD)
-        await expect(page.getByText(TEST_USER)).toBeVisible()
+        await pm.loginPage.login(validUser.email, validUser.password)
+        await expect(page.getByText(validUser.name)).toBeVisible()
 
         await page.getByRole('link', {name: 'Logout'}).click()
-        await expect(page.getByText('Login to your account')).toBeVisible();
+        await pm.loginPage.expectSuccessfulLogin();
 
     })
 
