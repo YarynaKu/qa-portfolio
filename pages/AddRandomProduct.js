39e1db26239828be4109afe2833cbd345a880794
YarynaKu <yaryna.kushniruk@gmail.com>
2026-02-06 15:25:56 +0100

    Added new methods verifyProdDetails, verifyProdinCart in AddRandomProduct.js

--- pages/AddRandomProduct.js
+++ pages/AddRandomProduct.js
@@ -9,12 +9,36 @@ export default class AddRandomProduct {
         }
 
         async clickRandomViewProduct() {
-            const products = this.page.locator('a', { hasText: 'View Product' })
+//            const products = this.page.locator('a', { hasText: 'View Product' })
+                const products = this.page.locator('div.product-image-wrapper')
                 const count = await products.count()
                 const randomIndex = Math.floor(Math.random() * count)
                 const selectedItem = products.nth(randomIndex)
 
-                await selectedItem.click()
+                const productName = await selectedItem.locator('div.productinfo p').innerText();
+                console.log('Selected product:', productName);
+                await selectedItem.locator('a:has-text("View Product")').click()
+
+                return { productName }
+        }
+
+        async verifyProductDetails(productName) {
+                const selectedItem = this.page.locator('div.product-information h2');
+                await selectedItem.waitFor({ state: 'visible' });
+                const titleText = await selectedItem.innerText();
+                console.log('Product title on page:', titleText);
+                console.log(`Selected product: ${productName}`);
+
+                return titleText.includes(productName);
+        }
+
+        async verifyProductInCart(productName) {
+                const cartItem = this.page.locator('#cart_info tbody tr').filter({ hasText: productName});
+                await expect(cartItem).toBeVisible();
+                const quantityInCart = this.page.locator('td.cart_quantity')
+                const quantityText = await quantityInCart.innerText();
+                console.log('Quantity in cart:', quantityText);
+                await expect(quantityInCart).toContainText('4')
         }
 
 }
