fe5a49284d888794e74963391ee2f375d555ec31
YarynaKu <yaryna.kushniruk@gmail.com>
2026-02-06 21:34:32 +0100

    Modified DeleteUser.js from function to a class

--- pages/DeleteUser.js
+++ pages/DeleteUser.js
@@ -1,9 +1,17 @@
 import { expect } from "@playwright/test";
+import CommonActions from "../utils/CommonActions.js";
 
-export async function deleteUser (page) {
+export default class DeleteUser{
+    constructor(page){
+        this.actions = new CommonActions(page)
+        this.page = page
 
-    await page.getByRole('link', {name: 'Delete Account'}).click()
-    await expect(page.getByText('Account Deleted!')).toBeVisible()
-    await page.getByRole('link', {name: 'Continue'}).click()
-    
+
+    }
+
+    async deleteUser(){
+        await this.page.getByRole('link', {name: 'Delete Account'}).click()
+        await expect(this.page.getByText('Account Deleted!')).toBeVisible()
+        await this.page.getByRole('link', {name: 'Continue'}).click()
+    }
 }
