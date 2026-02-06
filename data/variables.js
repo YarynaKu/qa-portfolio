221d10eaee1657b73e500638ea25244849c4cde8
YarynaKu <yaryna.kushniruk@gmail.com>
2026-02-06 15:24:14 +0100

    Added new variables 'products' in variables.js

--- data/variables.js
+++ data/variables.js
@@ -31,6 +31,15 @@ export let searchProducts = {
     searchedProduct: /Dress/
 };
 
+export const products = {
+    product1: {
+        id: 1, name: 'Blue Top', category: 'Women > Tops', price: 'Rs. 500', quantity: '1', total: 'Rs. 500'
+    },
+    product2: {
+        id:2, name: 'Men Tshirt', category: 'Men > Tshirts', price: 'Rs. 400', quantity: '1', total: 'Rs. 400'
+    }
+};
+
 export const categories = [
     { main: 'Women', sub: ['Dress', 'Tops', 'Saree'] },
     { main: 'Men', sub: ['TShirts', 'Jeans'] },
