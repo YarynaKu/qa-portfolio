8c0b7a2a2e1a45b420690f2a483c21d436a2cadc
YarynaKu <yaryna.kushniruk@gmail.com>
2026-02-09 19:49:30 +0100

    Modified TC15,16,17,23,24 according to POM

--- tests/OrderVerificationTests.spec.js
+++ tests/OrderVerificationTests.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test"
 import PomManager from "../pages/POM_practise";
 import { Payment } from "../pages/Payment";
 import { deleteUser } from "../pages/DeleteUser";
-import { products, validUser, registeredUser, payCart } from "../data/variables";
+import { products, validUser, registeredUser, payCart, expectedAddressFields } from "../data/variables";
 
 let pm;
 
@@ -159,7 +159,7 @@ test.describe("Orders Tests", () => {
 
     })
 
-    test.only("15 Place Order: Register before Checkout", async({page}) => {
+    test("15 Place Order: Register before Checkout", async({page}) => {
 
         await pm.menuBar.navigateToSignupLogin()
 
@@ -190,269 +190,209 @@ test.describe("Orders Tests", () => {
 
         await pm.productsPage.addRandomProduct()
 
-        await page.getByRole('link', {name: 'Cart'}).click()
+        await pm.productsPage.goToCartPopUpMessage()
 
-        await expect(page.locator('li[class="active"]')).toHaveText('Shopping Cart')
+        await pm.cartPage.verifyCartHeading()
 
-        await page.locator('a[class="btn btn-default check_out"]').click()
+        await pm.cartPage.clickCheckOutBtn()
 
-        await expect(page.getByRole('heading', {name: 'Address Details'})).toBeVisible()
+        await pm.cartPage.verifyAddressDetailsHeading()
 
-        await expect(page.getByRole('heading', {name: 'Review Your Order'})).toBeVisible()
+        await pm.cartPage.verifyReviewOrderHeading()
 
-        await page.locator('textarea[class="form-control"]').fill('Hello! Please put it in a gift bag')
-
-        await page.getByRole('link', {name: 'Place Order'}).click()
+        await pm.cartPage.writeCommentBox('Hello! Please put it in a gift bag')
 
         await test.step('Proceed with a payment', async() => {
 
-            await Payment (page, {
-                nameoncard: 'YaTest',
-                cardnumber: '1234567890',
-                cvc: '123',
-                expmonth: '01',
-                expyear: '01',
-            });
+            await pm.payment.enterPaymentDetails(
+                payCart.cardName,
+                payCart.cardNumber,
+                payCart.cvc,
+                payCart.expMonth,
+                payCart.expYear
+        )
 
-        await expect(page.locator('.alert-success')).toHaveCount(1)
-    })
+            await pm.payment.clickPayAndConfirm()
+            await pm.payment.verifyPaymentSuccess()
+        });
 
-        await deleteUser(page)
+        await pm.deleteUser.deleteUser()
     })
 
     test("16 Place Order: Login Before Checkout", async({page}) => {
 
-        await page.getByRole('link', {name:'Signup / Login'}).click()
+        await pm.menuBar.navigateToSignupLogin()
         await expect(page.getByText('Login to your account')).toBeVisible();
 
-        await pm.loginPage.login(`${TEST_EMAIL}`, `${TEST_PASSWORD}`)
-        await expect(page.getByText(TEST_USER)).toBeVisible()
+        await pm.loginPage.login(validUser.email, validUser.password)
+        await expect(page.getByText(validUser.name)).toBeVisible()
 
         await test.step('Adding products to the cart', async() => {
 
-            await page.locator('.product-image-wrapper').nth(0).hover()
-            await page.locator('div.overlay-content').locator('a[data-product-id="1"]').click()
+            await pm.productsPage.addRandomProduct()
         })
 
-        await page.getByRole('link', {name: 'Cart'}).click()
+        await pm.productsPage.goToCartPopUpMessage()
 
-        await expect(page.locator('li[class="active"]')).toHaveText('Shopping Cart')
+        await pm.cartPage.verifyCartHeading()
 
-        await page.locator('a[class="btn btn-default check_out"]').click()
+        await pm.cartPage.clickCheckOutBtn()
 
-        await expect(page.getByRole('heading', {name: 'Address Details'})).toBeVisible()
-        await expect(page.getByRole('heading', {name: 'Review Your Order'})).toBeVisible()
+        await pm.cartPage.verifyAddressDetailsHeading()
+        await pm.cartPage.verifyReviewOrderHeading()
 
-        await page.locator('textarea[class="form-control"]').fill('Hello! Please put it in a gift bag')
+        await pm.cartPage.writeCommentBox('Hello! Please put it in a gift bag')
 
-        await page.getByRole('link', {name: 'Place Order'}).click()
 
         await test.step('Proceed with a payment', async() => {
 
-            await Payment (page, {
-                nameoncard: 'YaTest',
-                cardnumber: '1234567890',
-                cvc: '123',
-                expmonth: '01',
-                expyear: '01',
-            });
+          await pm.payment.enterPaymentDetails(
+                   payCart.cardName,
+                   payCart.cardNumber,
+                   payCart.cvc,
+                   payCart.expMonth,
+                   payCart.expYear
+           )
 
-        await expect(page.locator('.alert-success')).toHaveCount(1)
+               await pm.payment.clickPayAndConfirm()
+               await pm.payment.verifyPaymentSuccess()
         })
 
-        await deleteUser(page)
+        await pm.deleteUser.deleteUser()
 
         })
 
 
     test("17 Remove Products from Cart", async({page}) => {
 
-                    await test.step('Adding products to the cart', async() => {
+            const product1 = await pm.productsPage.addRandomProduct()
+            await pm.productsPage.continueShoppingPopUpMessage()
 
-                        await page.locator('.product-image-wrapper').nth(0).hover()
-                        await page.locator('div.overlay-content').locator('a[data-product-id="1"]').click()
+            const product2 = await pm.productsPage.addRandomProduct()
+            await pm.productsPage.goToCartPopUpMessage()
 
-                        await page.getByRole('button', {name: 'Continue Shopping'}).click()
+            await pm.cartPage.verifyCartHeading()
 
-                        await page.locator('.product-image-wrapper').nth(1).hover()
-                        await page.locator('div.overlay-content').locator('a[data-product-id="2"]').click()
-                    })
+            await pm.productsPage.verifyProductsInCart(product1.productName)
+            await pm.productsPage.verifyProductsInCart(product2.productName)
 
-                    await page.getByRole('link', {name: 'Cart'}).click()
+            await pm.cartPage.deleteProductFromCart(product1.productName)
+            await pm.cartPage.deleteProductFromCart(product2.productName)
 
-                    await expect(page.locator('li[class="active"]')).toHaveText('Shopping Cart')
-
-                    await page.locator('[data-product-id="1"]').click()
-
-                    await expect(page.locator('[data-product-id="1"]')).toBeHidden()
-
-                })
+            })
 
     test("23 Verify address details in checkout page", async({page}) => {
 
+          await pm.menuBar.navigateToSignupLogin()
+          await expect(page.getByText('New User Signup!')).toBeVisible();
 
-        //        await page.getByRole('link', {name:'Signup / Login'}).click()
-        //        await expect(page.getByText('Login to your account')).toBeVisible();
-        //        await pm.loginPage.login(`${TEST_EMAIL}`, `${TEST_PASSWORD}`)
-        //        await expect(page.getByText(TEST_USER)).toBeVisible()
-        //        await page.getByRole('link', {name: 'Delete Account'}).click()
+          await pm.signupPage.signup(validUser.name, validUser.email)
 
+          await pm.registerUser.registration (page, {
 
-                await page.getByRole('link', {name:'Signup / Login'}).click()
-                await expect(page.getByText('New User Signup!')).toBeVisible();
+                        password: validUser.password,
+                        day: registeredUser.birthDate,
+                        month: registeredUser.birthMonth,
+                        year: registeredUser.birthYear,
+                        firstname: registeredUser.firstName,
+                        lastname: registeredUser.lastName,
+                        company: registeredUser.company,
+                        address: registeredUser.address,
+                        address2: registeredUser.address2,
+                        country: registeredUser.country,
+                        state: registeredUser.state,
+                        city: registeredUser.city,
+                        zipcode: registeredUser.zipcode,
+                        mobilenumber: registeredUser.mobileNumber,
 
-                await registerUser (page, {
-                    password: 'YaTestPassword!',
-                    firstname: 'YaTest',
-                    lastname: 'User',
-                    company: 'Google',
-                    address: 'Silicon Valley',
-                    address2: 'Guess what',
-                    state: 'Atlantica',
-                    city: 'Toronto',
-                    zipcode: '46971',
-                    mobilenumber: '0375839284',
-                });
+                    });
 
-                await page.getByRole('link', {name: 'Continue'}).click()
+          await page.getByRole('link', {name: 'Continue'}).click()
 
-                await expect(page.getByText(TEST_USER)).toBeVisible()
+          await expect(page.getByText(validUser.name)).toBeVisible()
 
-                const products = page.locator('.product-image-wrapper .overlay-content')
-                const count = await products.count()
-                        const randomIndex = Math.floor(Math.random() * count)
-                        const selectedItem = products.nth(randomIndex)
-                        const productName = await selectedItem.locator('p').innerText()
+                await pm.productsPage.addRandomProduct()
+                await pm.productsPage.continueShoppingPopUpMessage()
 
-                await page.waitForTimeout(5000)
+                await pm.productsPage.addRandomProduct()
+                await pm.productsPage.goToCartPopUpMessage()
 
-                await selectedItem.locator('a.add-to-cart').click()
+                await pm.cartPage.verifyCartHeading()
 
-                await page.locator('.modal-content').locator('a[href="/view_cart"]').click()
-                await expect(page.locator('#cart_info_table td.cart_description h4 a', {hasText: productName })).toBeVisible()
+                await pm.cartPage.clickCheckOutBtn()
 
-                await page.locator('#do_action .btn.btn-default.check_out').click()
+                await pm.cartPage.verifyDeliveryAddress(registeredUser)
 
-                const deliveryAddress = page.locator('#address_delivery')
+                await pm.cartPage.verifyBillingAddress(registeredUser)
 
-                await expect(deliveryAddress).toContainText(/your delivery address/i)
-                await expect(deliveryAddress).toContainText(/Mrs\. YaTest User/i)
-                await expect(deliveryAddress).toContainText(/Google/i)
-                await expect(deliveryAddress).toContainText(/Silicon Valley/i)
-                await expect(deliveryAddress).toContainText(/Guess what/i)
-                await expect(deliveryAddress).toContainText(/Toronto Atlantica/i)
-                await expect(deliveryAddress).toContainText(/46971/i)
-                await expect(deliveryAddress).toContainText(/Canada/i)
-                await expect(deliveryAddress).toContainText(/0375839284/i)
-
-                const billingAddress = page.locator('#address_invoice')
-
-                await expect(billingAddress).toContainText(/your billing address/i)
-                await expect(billingAddress).toContainText(/Mrs\. YaTest User/i)
-                await expect(billingAddress).toContainText(/Google/i)
-                await expect(billingAddress).toContainText(/Silicon Valley/i)
-                await expect(billingAddress).toContainText(/Guess what/i)
-                await expect(billingAddress).toContainText(/Toronto Atlantica/i)
-                await expect(billingAddress).toContainText(/46971/i)
-                await expect(billingAddress).toContainText(/Canada/i)
-                await expect(billingAddress).toContainText(/0375839284/i)
-
-                await page.getByRole('link', {name: 'Delete Account'}).click()
+                await pm.deleteUser.deleteUser()
 
             })
 
 
-    test("24 Download Invoice after purchase order", async ({page}) => {
+    test.only("24 Download Invoice after purchase order", async ({page}) => {
 
-                    const products = page.locator('.product-image-wrapper .overlay-content')
+                const product = await pm.productsPage.addRandomProduct()
+                await pm.productsPage.continueShoppingPopUpMessage()
 
-                    const count = await products.count()
-                          const randomIndex = Math.floor(Math.random() * count)
-                          const selectedItem = products.nth(randomIndex)
-                          const productName = await selectedItem.locator('p').innerText()
+                await pm.productsPage.addRandomProduct()
+                await pm.productsPage.goToCartPopUpMessage()
 
-                    await products.first().waitFor({ state: 'visible' });
+                await pm.cartPage.verifyCartHeading()
 
-                    await selectedItem.locator('a.add-to-cart').evaluate(el => el.click())
+                await pm.cartPage.clickCheckOutBtn()
 
-                    await page.locator('.modal-content').locator('a[href="/view_cart"]').click()
+                await pm.cartPage.clickRegisterPopupCheckout()
 
-                    await expect(page.getByText("Shopping Cart")).toBeVisible()
+                await pm.signupPage.signup(validUser.name, validUser.email)
 
-                    await page.locator('a[class="btn btn-default check_out"]').click()
+                await pm.registerUser.registration (page, {
 
-                    await page.locator('.modal-content').locator('a[href="/login"]').click()
+                     password: validUser.password,
+                     day: registeredUser.birthDate,
+                     month: registeredUser.birthMonth,
+                     year: registeredUser.birthYear,
+                     firstname: registeredUser.firstName,
+                     lastname: registeredUser.lastName,
+                     company: registeredUser.company,
+                     address: registeredUser.address,
+                     address2: registeredUser.address2,
+                     country: registeredUser.country,
+                     state: registeredUser.state,
+                     city: registeredUser.city,
+                     zipcode: registeredUser.zipcode,
+                     mobilenumber: registeredUser.mobileNumber,
+                    });
 
-                    await page.getByPlaceholder('Name').fill(TEST_USER)
-                    await page.locator('input[data-qa="signup-email"]').fill(TEST_EMAIL)
+                await page.getByRole('link', {name: 'Continue'}).click()
+                await expect(page.getByText(validUser.name)).toBeVisible()
 
-                    await page.getByRole('button', {name: 'Signup'}).click()
+                await pm.menuBar.navigateToCart()
+                await pm.cartPage.clickCheckOutBtn()
 
-                    await registerUser (page, userData);
+                await pm.cartPage.verifyDeliveryAddress(registeredUser)
 
-                    await page.getByRole('link', {name: 'Continue'}).click()
-                    await expect(page.getByText(TEST_USER)).toBeVisible()
+                await pm.cartPage.verifyBillingAddress(registeredUser)
 
-                    await page.getByRole('link', {name: 'Cart'}).click()
-                    await page.locator('a[class="btn btn-default check_out"]').click()
+                await pm.cartPage.writeCommentBox('Please, handle with care. Fragile item.')
 
-                    const deliveryAddress = page.locator('#address_delivery')
+                await pm.payment.enterPaymentDetails (
+                    payCart.cardName,
+                    payCart.cardNumber,
+                    payCart.cvc,
+                    payCart.expMonth,
+                    payCart.expYear
+                );
 
-                    expect(deliveryAddress).toContainText(userData.firstname)
-                    expect(deliveryAddress).toContainText(userData.lastname)
-                    expect(deliveryAddress).toContainText(userData.company)
-                    expect(deliveryAddress).toContainText(userData.address)
-                    expect(deliveryAddress).toContainText(userData.address2)
-                    expect(deliveryAddress).toContainText(userData.city)
-                    expect(deliveryAddress).toContainText(userData.state)
-                    expect(deliveryAddress).toContainText(userData.zipcode)
-                    expect(deliveryAddress).toContainText(userData.mobilenumber)
+                await pm.payment.clickPayAndConfirm()
+                await pm.payment.verifyPaymentSuccess()
 
-                    const billingAddress = page.locator('#address_invoice')
+                await pm.payment.downloadInvoice()
 
-                    expect(billingAddress).toContainText(userData.firstname)
-                    expect(billingAddress).toContainText(userData.lastname)
-                    expect(billingAddress).toContainText(userData.company)
-                    expect(billingAddress).toContainText(userData.address)
-                    expect(billingAddress).toContainText(userData.address2)
-                    expect(billingAddress).toContainText(userData.city)
-                    expect(billingAddress).toContainText(userData.state)
-                    expect(billingAddress).toContainText(userData.zipcode)
-                    expect(billingAddress).toContainText(userData.mobilenumber)
+                await page.getByRole('link', {name: 'Continue'}).click()
 
-                    await expect(page.locator('#cart_info td.cart_description h4 a', {hasText: productName })).toBeVisible()
-
-                    await page.locator('textarea[name="message"]').fill('Please, handle with care. Fragile item.')
-
-                    await page.getByRole('link', {name: 'Place Order'}).click()
-
-                        await Payment (page, {
-                            nameoncard: 'YaTest',
-                            cardnumber: '1234567890',
-                            cvc: '123',
-                            expmonth: '01',
-                            expyear: '01',
-                        });
-
-                    await page.getByRole('button', {name: 'Pay and Confirm Order'}).click()
-                    await expect(page.locator('.alert-success')).toHaveCount(1)
-
-                    await page.getByRole('link', {name: 'Download Invoice'}).click()
-
-                    const downloadPromise = page.waitForEvent('download');
-
-                    const download = await downloadPromise;
-
-                    expect(download.suggestedFilename()).toContain('invoice');
-
-                    const filePath = `downloads/${download.suggestedFilename()}`;
-                    await download.saveAs(filePath);
-
-                    await page.getByRole('link', {name: 'Continue'}).click()
-
-                    await page.getByRole('link', {name: 'Delete Account'}).click()
+                await pm.deleteUser.deleteUser()
 
                 })
-
 
 })
