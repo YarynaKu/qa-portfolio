354620c11068da198aedc88f5bf358ee4979a8af
YarynaKu <yaryna.kushniruk@gmail.com>
2026-02-09 19:46:30 +0100

    Added new methods deleteProduct, verify DeliveryAddress, verifyBillingAddress to CartPage.js

--- pages/CartPage.js
+++ pages/CartPage.js
@@ -1,5 +1,7 @@
 import { expect } from "@playwright/test";
 import CommonActions from "../utils/CommonActions.js";
+import { expectedAddressFields } from "../data/variables.js"
+import { normalizeText } from "../utils/utils.js"
 
 export default class CartPage {
         constructor(page){
@@ -37,6 +39,45 @@ export default class CartPage {
     async writeCommentBox(review){
         await this.actions.fill(this.commentBoxLocator, review)
         await this.page.getByRole('link', {name: 'Place Order'}).click()
+    }
+
+    async deleteProductFromCart(productName) {
+
+        const cartItem = this.page.locator('#cart_info tbody tr').filter({ hasText: productName});
+        await expect(cartItem).toBeVisible();
+
+        await cartItem.locator('.cart_quantity_delete').click()
+        await expect(cartItem).toBeHidden()
+    }
+
+
+    async verifyDeliveryAddress(registeredUser) {
+        const deliveryAddress = this.page.locator('#address_delivery')
+
+        const deliveryText = (await deliveryAddress.innerText())
+                                 .split('\n')
+                                 .map(line => normalizeText(line))
+                                 .filter(Boolean);
+
+        const deliveryFullText = deliveryText.join(' ');
+        for (const field of expectedAddressFields) {
+            expect(deliveryFullText).toContain(field)
+        }
+    }
+
+    async verifyBillingAddress(){
+       const billingAddress = this.page.locator('#address_invoice')
+
+       const invoiceText = (await billingAddress.innerText())
+                                .split('\n')
+                                .map(line => normalizeText(line))
+                                .filter(Boolean);
+
+       const invoiceFullText = invoiceText.join(' ');
+
+       for (const field of expectedAddressFields) {
+             expect(invoiceFullText).toContain(field)
+       }
     }
 
 }
