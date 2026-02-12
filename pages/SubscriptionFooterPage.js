6c903bc390e5424d1b29e13a27c943dc6543ed47
YarynaKu <yaryna.kushniruk@gmail.com>
2026-02-12 22:01:21 +0100

    Added method nabigateToFooter

--- pages/SubscriptionFooterPage.js
+++ pages/SubscriptionFooterPage.js
@@ -4,17 +4,23 @@ import CommonActions from "../utils/CommonActions.js";
 export default class SubscriptionFooter {
 
     constructor(page) {
-        this.actions = new CommonActions(page)
+        this.page = page;
 
         this.subscriptionFooter = page.getByRole('heading', {name: 'Subscription'})
-        this.subscriptionInputSelector = '#susbscribe_email'
-        this.subscribeButtonSelector = '#subscribe'
+        this.subscriptionInputSelector = page.locator('#susbscribe_email')
+        this.subscribeButtonSelector = page.locator('#subscribe')
         this.successMessage = page.getByText('You have been successfully subscribed!')
+
+        this.footerText = page.getByText('Copyright © 2021 All rights reserved')
+        this.scrollArrow = page.locator('#scrollUp')
+        this.verifyTopText = page.locator('#slider-carousel .carousel-inner h2', {hasText: 'Full-Fledged practice website for Automation Engineers'})
+
+        this.headerText = page.locator('#header')
     }
 
     async subscribe(email){
-        await this.actions.fill(this.subscriptionInputSelector, email)
-        await this.actions.click(this.subscribeButtonSelector)
+        await this.subscriptionInputSelector.fill(email)
+        await this.subscribeButtonSelector.click()
     }
 
     async expectSuccessfulSubscription(){
@@ -23,6 +29,22 @@ export default class SubscriptionFooter {
 
     async expectSubscriptionFooter(){
         await expect(this.subscriptionFooter).toBeVisible()
+    }
+
+    async navigateToFooterText(){
+        await this.footerText.scrollIntoViewIfNeeded()
+    }
+
+    async scrollUpWithArrow(){
+        await this.scrollArrow.click()
+    }
+
+    async verifySuccessTopText(){
+        await expect(this.verifyTopText.first()).toBeVisible()
+    }
+
+    async navigateToHeaderText(){
+        await this.headerText.scrollIntoViewIfNeeded()
     }
 
 }
