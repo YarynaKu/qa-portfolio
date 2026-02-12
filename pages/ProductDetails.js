61d7a305cc7843c06b02e3ddbf0dc592a8156f77
YarynaKu <yaryna.kushniruk@gmail.com>
2026-02-12 22:00:09 +0100

    added new method verifyProdDetails

--- pages/ProductDetails.js
+++ pages/ProductDetails.js
@@ -1,16 +1,17 @@
 import { expect } from '@playwright/test';
-import CommonActions from '../utils/CommonActions.js';
+import ProductsPage from "./ProductsPage.js"
 
 export default class ProductDetails {
     constructor(page) {
-        this.actions = new CommonActions(page);
+
         this.page = page;
 
-        this.writeReviewSelector = 'a[href="#reviews"]'
+        this.writeReviewSelector = page.locator('a[href="#reviews"]')
         this.thanksMessage = page.getByText('Thank you for your review.')
-        this.quantitySelector = '#quantity'
+        this.quantitySelector = page.locator('#quantity')
 
-    }
+        this.productInfo = page.locator('div.product-information')
+        }
 
     async verifyWriteReviewSection() {
         await expect(this.page.locator(this.writeReviewSelector)).toBeVisible();
@@ -28,11 +29,26 @@ export default class ProductDetails {
     }
 
     async setProductQuantity(quantity) {
-        await this.actions.fill(this.quantitySelector, quantity);
+        await this.quantitySelector.fill(quantity);
     }
 
     async addToCart() {
         await this.page.getByRole('button', { name: 'Add to cart' }).click();
     }
+
+    async verifyProdInfoDetails(productId, expected){
+        await expect(this.productInfo).toBeVisible();
+
+        const prodDetails = this.page.locator('div.product-information')
+
+        await expect(prodDetails.locator('h2')).toHaveText(expected.name);
+        await expect(prodDetails.locator('p:has-text("Category")')).toContainText(expected.category);
+        await expect(prodDetails.locator('span span:has-text("Rs.")')).toHaveText(expected.price);
+        await expect(prodDetails.locator('input[type="number"]')).toHaveValue(expected.quantity);
+        await expect(prodDetails.locator('p:has(b:has-text("Availability"))')).toContainText(expected.availability);
+        await expect(prodDetails.locator('p:has(b:has-text("Condition"))')).toContainText(expected.condition);
+        await expect(prodDetails.locator('p:has(b:has-text("Brand"))')).toContainText(expected.brand);
+    }
+
 
 }
